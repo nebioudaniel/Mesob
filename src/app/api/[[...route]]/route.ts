2de21750bedb779c730f5e2f8661b2d26b4d4669
Nebiou Daniel Berhane <124292845+nebioudaniel@users.noreply.github.com>
2025-05-23 20:14:10 +0300

    Update route.ts

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,5 +1,5 @@
 import { httpHandler } from "@/server"
 
-export const runtime = "edge"
+// export const runtime = "edge"  <-- remove or comment this out
 
 export { httpHandler as GET, httpHandler as POST }
